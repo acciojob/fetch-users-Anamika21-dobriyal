ee4ce4df60204761437bf863cc0c057086f32776
Anamika <anamikadobriyal21@gmail.com>
2025-04-06 22:05:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
+import UserList from "./components/UserList";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
